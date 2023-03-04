bc9d7b0a73f5104c76820608e4ed866c1efbd564
zZixAxX <96460824+zZixAxX@users.noreply.github.com>
2023-03-04 11:09:11 -0500

    Player Input
    More player modification

--- Hellcore/Assets/Player/MouseMovement.cs
+++ Hellcore/Assets/Player/MouseMovement.cs
@@ -1,33 +1,99 @@
 using Palmmedia.ReportGenerator.Core.Reporting.Builders;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MouseMovement : MonoBehaviour
 {
+    private PlayerInputAction playerInputAction;
+
     public float mouseSensitivityX = 500f;
-    public float mouseSensitivityY = 80f;
+    public float mouseSensitivityY = 300f;
 
-    public Transform playerBody;
-
+    public Vector2 mouseLook;
+    
     float xRotation = 0f;
+
+    private Transform playerBody;
+
     void Start()
     {
+        
+    }
+
+    private void Awake()
+    {
+        playerBody = transform.parent;
+
+        playerInputAction = new PlayerInputAction();
+
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnEnable()
+    {
+        playerInputAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputAction.Disable();
     }
 
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY * Time.deltaTime;
+        mouseLook = playerInputAction.PlayerMovement.Look.ReadValue<Vector2>();
+
+        float mouseX = mouseLook.x * mouseSensitivityX * Time.deltaTime;
+        float mouseY = mouseLook.y * mouseSensitivityY * Time.deltaTime;
 
         xRotation -= mouseY;
-        //clamp calcule l'axe selon max/min
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
-
     }
 }
+
+//playerBody.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+
+
+//Reset
+//if(mouseX >= 1f || mouseX <= -1f)
+//    mouseX = 0f;
+//if(mouseY >= 1f || mouseX <= -1f)
+//    mouseX = 0f;
+//mouseY = 0f;
+//if (timer < resetTime)
+//    timer += Time.deltaTime;
+//else
+//{
+//    mouseX = 0f;
+//    mouseY = 0f;
+//}
+
+//public void Look(InputAction.CallbackContext context)
+//{
+//    Vector2 inputVector = context.ReadValue<Vector2>();
+//    if (context.performed)
+//    {
+//        mouseX = inputVector.x;
+//        mouseY = inputVector.y;
+//    }
+//    //timer = 0f;
+//    ////else if (context.canceled)
+//    ////{
+//    ////    mouseX = 0f;
+//    ////    mouseY = 0f;
+//    ////}
+//    //Debug.Log(mouseX);
+
+//    //xRotation -= mouseY;
+//    //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+//    //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+//    //playerBody.Rotate(Vector3.up * mouseX);
+//}
